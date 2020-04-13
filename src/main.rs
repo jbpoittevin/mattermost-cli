a6c62d0b4bb2532410d7a97ecbd2bfb4d5a7c284
Jean-Baptiste Poittevin <jean-baptiste.poittevin@dgfip.finances.gouv.fr>
2020-04-13 11:47:02 +0200

    fix typing issues with serde_json

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut headers = HashMap::new();
     headers.insert("Accept".to_string(), "application/json".to_string());
 
+    let uri = "http://salon.noisy.dgfip/api/v4/users/login";
     let client = reqwest::Client::new();
-    let res = client.post("http://salon.noisy.dgfip/api/v4/users/login")
+    let res: serde_json::Value = client.post(uri)
         .json(&headers)
         .send()
         .await?
-        .json::<HashMap<String, String>>()
+        .json()
         .await?;
 
     println!("{:#?}", res);
